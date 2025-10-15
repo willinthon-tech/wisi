4e58f7299d85ea54a22ad7847180edfb0cabada2
willinthon-tech <anthonycarriedo2013@gmail.com>
2025-10-14 22:30:52 -0400

    fino hasta aca

--- server.js
+++ server.js
@@ -801,7 +801,8 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(helmet());
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '500mb' }));
+app.use(express.urlencoded({ limit: '500mb', extended: true }));
 
 
 // Rate limiting DESHABILITADO para desarrollo
