a413186856a2e807a69761b64b74d97272af41ac
willinthon-tech <anthonycarriedo2013@gmail.com>
2025-10-18 05:58:14 -0400

    deploy

--- wisi-frontend/src/environments/environment.prod.ts
+++ wisi-frontend/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //apiUrl: 'https://wisi.space/api'
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://wisi.space/api'
+  //apiUrl: 'http://localhost:3000/api'
 };
