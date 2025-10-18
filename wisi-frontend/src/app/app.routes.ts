ae5623de3eae7de2937405f3037d149e4dfbb105
willinthon-tech <anthonycarriedo2013@gmail.com>
2025-10-17 20:37:31 -0400

    fino hasta aca

--- wisi-frontend/src/app/app.routes.ts
+++ wisi-frontend/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { AuthGuard } from './guards/auth.guard';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/login',
+    redirectTo: '/dashboard',
     pathMatch: 'full'
   },
   {
