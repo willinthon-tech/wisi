b9c54eb7b98f3b3ee5435d22cec0fe3420096b3d
willinthon-tech <anthonycarriedo2013@gmail.com>
2026-01-21 05:30:57 -0400

    cambios en conexion a mysql y campo doorNo en ISAPI

--- models/index.js
+++ models/index.js
@@ -8,6 +8,12 @@ const sequelize = new Sequelize({
   logging: false, // Cambiar a console.log para ver las consultas SQL
 });
 
+/* const sequelize = new Sequelize('wisi', 'root', '0p0rt3R0y', {
+  host: 'localhost', // o la IP de tu servidor MySQL
+  dialect: 'mysql',
+  logging: false, // Opcional, para limpiar la consola
+}); */
+
 // Importar modelos
 const User = require('./User')(sequelize);
 const Sala = require('./Sala')(sequelize);
--- server.js
+++ server.js
@@ -9492,6 +9492,7 @@ app.post('/api/tareas/dispositivo/agregar-usuario', authenticateToken, async (re
           name: tarea.nombre_empleado,
           gender: tarea.nombre_genero,
           userType: 'normal',
+          doorNo: 1, // <--- AGREGA ESTA LÍNEA AQUÍ BIOMETRICOS NUEVOS AL PARECER
           localUIRight: false,
           maxOpenDoorTime: 0,
           Valid: {
@@ -9597,6 +9598,7 @@ app.post('/api/tareas/dispositivo/editar-usuario', authenticateToken, async (req
           name: tarea.nombre_empleado,
           gender: tarea.nombre_genero,
           userType: 'normal',
+          doorNo: 1, // <--- AGREGA ESTA LÍNEA AQUÍ BIOMETRICOS NUEVOS AL PARECER
           Valid: {
             enable: true,
             beginTime: tarea.marcaje_empleado_inicio_dispositivo || tarea.fecha_ingreso || '2024-01-01T00:00:00',
