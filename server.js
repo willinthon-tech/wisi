7fe64be7c13a425d6bcff4fbe2bca2be155b095c
willinthon-tech <anthonycarriedo2013@gmail.com>
2026-01-21 05:59:43 -0400

    test 2

--- server.js
+++ server.js
@@ -9485,15 +9485,14 @@ app.post('/api/tareas/dispositivo/agregar-usuario', authenticateToken, async (re
         }
       };
     } else {
-      // Estructura para BIOMÉTRICO
-      console.log('hooola')
+      // Estructura para BIOMÉTRICO 
       body = {
         UserInfo: {
           employeeNo: tarea.numero_cedula_empleado,
           name: tarea.nombre_empleado,
           gender: tarea.nombre_genero,
           userType: 'normal',
-          doorNo: 1, // <--- AGREGA ESTA LÍNEA AQUÍ BIOMETRICOS NUEVOS AL PARECER
+          doorNo: "1", // <--- AGREGA ESTA LÍNEA AQUÍ BIOMETRICOS NUEVOS AL PARECER
           localUIRight: false,
           maxOpenDoorTime: 0,
           Valid: {
@@ -9599,7 +9598,7 @@ app.post('/api/tareas/dispositivo/editar-usuario', authenticateToken, async (req
           name: tarea.nombre_empleado,
           gender: tarea.nombre_genero,
           userType: 'normal',
-          doorNo: 1, // <--- AGREGA ESTA LÍNEA AQUÍ BIOMETRICOS NUEVOS AL PARECER
+          doorNo: "1", // <--- AGREGA ESTA LÍNEA AQUÍ BIOMETRICOS NUEVOS AL PARECER
           Valid: {
             enable: true,
             beginTime: tarea.marcaje_empleado_inicio_dispositivo || tarea.fecha_ingreso || '2024-01-01T00:00:00',
